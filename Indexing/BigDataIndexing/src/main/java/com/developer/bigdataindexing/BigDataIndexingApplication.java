b3c8987df36ed31fd431558cdab09e535816add2
Shubham Jain <jain.shub@northeastern.edu>
2020-10-09 11:42:53 -0400

    Added Demo1 files

---
+++ Indexing/BigDataIndexing/src/main/java/com/developer/bigdataindexing/BigDataIndexingApplication.java
@@ -0,0 +1,13 @@
+package com.developer.bigdataindexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataIndexingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataIndexingApplication.class, args);
+	}
+
+}
